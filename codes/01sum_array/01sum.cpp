bc6f0b3c0f3d02b6c235a20fa7c9072e7000de4c
黄重庆 <chongqing.huang@waytous.com>
2022-01-31 01:27:07 +0800

    add:OpenMP

--- codes/01sum_array/01sum.cpp
+++ codes/01sum_array/01sum.cpp
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2022-01-31 00:05:38
- * @LastEditTime: 2022-01-31 00:51:13
+ * @LastEditTime: 2022-01-31 01:26:31
  * @FilePath: /HPC/codes/01sum_array/01sum.cpp
  */
 
@@ -13,12 +13,14 @@
 #include <vector>
 #include <algorithm> //sort,reverse
 //
+#include <omp.h> //OpenMP—for指令法求和
+
 #include <cmath> //   pow(a,b);//a必须是double类型的，b是a的多少次方 ! !
 using namespace std;
 #include <chrono>       // 时间must
 using namespace chrono; // 时间must
 
-int sum_array(vector<double>& arr, int len)
+int sum_array_normal(vector<double>& arr, int len)
 {
     int sum = 0;
     for (int i = 0; i < len; ++i)
@@ -28,25 +30,51 @@ int sum_array(vector<double>& arr, int len)
     }
     return sum;
 }
+// 
+int sum_array_openmp(vector<double>& arr, int len)
+{
+    int sum = 0;
+    
+    #pragma omp parallel
+	{
+    #pragma omp for reduction(+:sum) // 每个线程拷贝一份sum变量，退出并行块时再把各个线程的sum相加
+        for (int i = 0; i < len; ++i)
+        {
+            arr[i] = i;    // 赋值
+            sum += arr[i]; // 求和
+        }
+    }
+    return sum;
+}
 
 int main()
 {
     std::cout << "## Starting caculate ...  "<< std::endl;
     auto t1 = std::chrono::steady_clock::now();
     // ======================================2^28长度的vector做赋值跟求和，计算一下时间
-    // 输入给一个2^28长度的vector做赋值跟求和，计算一下时间
+    // 输入: 给一个2^28长度的vector做赋值跟求和，计算一下时间
     int sum = 0;
     const int len = pow(2.0, 28);
     vector<double> a(len); // 定义ector
-    sum = sum_array(a, len);
+
+    // 处理1 
+    sum = sum_array_normal(a, len);
     // =====================================
     auto t2 = std::chrono::steady_clock::now();
     double dr_ms = std::chrono::duration<double, std::milli>(t2 - t1).count(); //这一帧处理时间  毫秒ms
-    std::cout << "## Extract  time: " << dr_ms << " ms， result: " << sum << std::endl;
-    // 处理
+    std::cout << "## 【normal】Extract  time: " << dr_ms << " ms， Result: " << sum << std::endl;
 
-    //输出
-    cout << "time" << endl;
+    // 处理2: OpenMP//设置线程数，一般设置的线程数不超过CPU核心数，这里开4个线程执行并行代码段
+	// omp_set_num_threads(4);
+    // #pragma omp parallel
+	// {
+	// 	cout << "Hello" << ", I am Thread " << omp_get_thread_num() << endl;
+	// }
+    sum = 0;
+    sum = sum_array_openmp(a, len);
 
+    auto t3 = std::chrono::steady_clock::now();
+    double dr_ms1 = std::chrono::duration<double, std::milli>(t3 - t2).count(); //这一帧处理时间  毫秒ms
+    std::cout << "##  【OpenMP】Extract  time: " << dr_ms1 << " ms， Result: " << sum << std::endl;
     return 0;
 }
